9f10c2da4702555f44920ec5dc802c7ec72f7f2a
Witek85 <witold.szklarski@gmail.com>
2017-10-07 23:48:47 +0200

    customers component 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CustomersComponent } from './customers/customers.component';
+import { CustomerListComponent } from './customers/customer-list/customer-list.component';
+import { CustomerDetailComponent } from './customers/customer-detail/customer-detail.component';
+import { CustomerItemComponent } from './customers/customer-list/customer-item/customer-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CustomersComponent    
+    CustomersComponent,
+    CustomerListComponent,
+    CustomerDetailComponent,
+    CustomerItemComponent    
   ],
   imports: [
     BrowserModule,
