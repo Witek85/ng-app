069c1a0172e431fa7344773be32746c8fb836acd
Witek85 <witold.szklarski@gmail.com>
2017-10-12 23:41:23 +0200

    customer

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from '../customer.model';
 
 @Component({
   selector: 'app-customer-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.css']
 })
 export class CustomerListComponent implements OnInit {
+	customers:Customer[] = [
+	new Customer(1,'Kowalski','Jan'),
+	new Customer(2,'Kowalska','Janina'),
+	new Customer(3,'Nowak','Jan')
+	];
 
   constructor() { }
 
---
+++ src/app/customers/customer.model.ts
@@ -0,0 +1,11 @@
+export class Customer {
+	public ID:number;
+	public name:string;
+	public firstName:string;
+	
+	constructor(ID:number, name:string, firstName:string) {
+		this.ID = ID;
+		this.name = name;
+		this.firstName = firstName;
+	}
+}
