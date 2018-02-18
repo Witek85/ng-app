520e4dd8788c0f63df0c3e6ff3b184c43640ebc0
Witek85 <witold.szklarski@gmail.com>
2018-02-18 23:36:30 +0100

    ingredients in recipe detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,28 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
 	recipeSelected = new EventEmitter<Recipe>();
 
 	private recipes: Recipe[] = [
-	new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/800px-Wiener-Schnitzel02.jpg'),
-	new Recipe('A Test Recipe 2', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg/800px-NYC-Diner-Bacon-Cheeseburger.jpg')
+	new Recipe(
+		'A Test Recipe', 
+		'This is simply a test', 
+		'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/800px-Wiener-Schnitzel02.jpg',
+		[
+		new Ingredient('Meat',1),
+		new Ingredient('Fries',20)
+		]),
+	new Recipe('A Test Recipe 2', 
+		'This is simply a test', 
+		'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg/800px-NYC-Diner-Bacon-Cheeseburger.jpg',
+		[
+		new Ingredient('Meat',1),
+		new Ingredient('Bun',1)
+		])
 	];
 
 	getRecipes() {
